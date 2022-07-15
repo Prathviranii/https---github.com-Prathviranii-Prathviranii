00b6476c4e757da7c1ca3fbbe493889864cd466f
Prathviranii <prathvirani2001@gmail.com>
2022-07-15 21:58:13 +0530

    commit 15/07/2022

---
+++ onmouseup.js
@@ -0,0 +1,6 @@
+<div id="dis1" style=" position: relative; right: 0px; top: 10px; background-color: #f1f1f1; 
+width: 250px;
+ padding: 10px; color: black; border: #0000cc 2px dashed; "
+ onmousedown="this.style.background='yellow';" 
+onmouseup="this.style.background='PowderBlue';">
+Bring Mouse inside this <br>Press and hold your mouse key first , then release it</div>
